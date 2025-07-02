c549fe7929ba822d884eeb1644c2a4a3c2770c16
Amir Merchad <amir.merchad@tu-dortmund.de>
2025-07-03 00:59:06 +0200

    restart button with its function

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ let player2choice = "";
 let round = 0;
 const p1score = document.querySelector(".p1-score");
 const p2score = document.querySelector(".p2-score");
-const winner = document.querySelector(".status");
+const win = document.querySelector(".status");
 const p1Weapon = document.querySelector(".p1-weapon");
 const p2Weapon = document.querySelector(".p2-weapon");
+const winner = document.querySelector(".winner");
 
 function getPlayer1Choice(name) {
     if (name === "Computer") {
@@ -43,96 +44,118 @@ function getPlayer2Choice(name) {
 }
 
 function rock(){
-    if (turn % 2 !== 0) {
-        player1choice = "rock";
-        p1Weapon.textContent = "✊";
-        turn++;
-        player2choice = getPlayer2Choice("Computer");
-        turn++;
-        playGame(player1choice, player2choice)
+    if (player1Score < 5 && player2Score < 5) {
+        if (turn % 2 !== 0) {
+            player1choice = "rock";
+            p1Weapon.textContent = "✊";
+            turn++;
+            player2choice = getPlayer2Choice("Computer");
+            turn++;
+            playGame(player1choice, player2choice)
+        } else {
+            getPlayer1Choice("Computer");
+            turn++;
+            player2choice = "rock";
+            p2Weapon.textContent = "✊";
+            turn++;
+            playGame(player1choice, player2choice)
+        }
     } else {
-        getPlayer1Choice("Computer");
-        turn++;
-        player2choice = "rock";
-        p2Weapon.textContent = "✊";
-        turn++;
-        playGame(player1choice,player2choice)
+        winner.textContent = "Press restart to play again.";
     }
 }
 
 function paper(){
-    if (turn % 2 !== 0) {
-        player1choice = "paper";
-        p1Weapon.textContent = "✋";
-        turn++;
-        player2choice = getPlayer2Choice("Computer");
-        turn++;
-        playGame(player1choice, player2choice)
+    if (player1Score < 5 && player2Score < 5) {
+        if (turn % 2 !== 0) {
+            player1choice = "paper";
+            p1Weapon.textContent = "✋";
+            turn++;
+            player2choice = getPlayer2Choice("Computer");
+            turn++;
+            playGame(player1choice, player2choice)
+        } else {
+            getPlayer1Choice("Computer");
+            turn++;
+            player2choice = "paper";
+            p2Weapon.textContent = "✋";
+            turn++;
+            playGame(player1choice,player2choice)
+        }
     } else {
-        getPlayer1Choice("Computer");
-        turn++;
-        player2choice = "paper";
-        p2Weapon.textContent = "✋";
-        turn++;
-        playGame(player1choice,player2choice)
+        winner.textContent = "Press restart to play again.";
     }
 }
 
 function scissors(){
-    if (turn % 2 !== 0) {
-        player1choice = "scissors";
-        p1Weapon.textContent = "✌️";
-        turn++;
-        player2choice = getPlayer2Choice("Computer");
-        turn++;
-        playGame(player1choice, player2choice)
+    if (player1Score < 5 && player2Score < 5) {
+        if (turn % 2 !== 0) {
+            player1choice = "scissors";
+            p1Weapon.textContent = "✌️";
+            turn++;
+            player2choice = getPlayer2Choice("Computer");
+            turn++;
+            playGame(player1choice, player2choice)
+        } else {
+            getPlayer1Choice("Computer");
+            turn++;
+            player2choice = "scissors";
+            p2Weapon.textContent = "✌️";
+            turn++;
+            playGame(player1choice, player2choice)
+        }
     } else {
-        getPlayer1Choice("Computer");
-        turn++;
-        player2choice = "scissors";
-        p2Weapon.textContent = "✌️";
-        turn++;
-        playGame(player1choice,player2choice)
+        winner.textContent = "Press restart to play again.";
     }
 }
 
 function resetGame() {
-    round = 0;
+    player1Score = 0;
+    player2Score = 0;
+    p1score.textContent = "Player 1: " + player1Score.toString();
+    p2score.textContent = "Player 2: " + player2Score.toString();
 }
 
 function playRound(player1Choice, player2Choice) {
     if (player1Choice === player2Choice) {
-        winner.textContent = "It's a tie!";
+        win.textContent = "It's a tie!";
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
+        winner.textContent = "No one wins this round!";
     } else if (player1Choice === "rock" && player2Choice === "scissors"){
-        winner.textContent = "Player 1 wins!";
+        win.textContent = "Player 1 wins!";
         player1Score++;
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
+        winner.textContent = "rock crushes scissors!";
     } else if(player1Choice === "paper" && player2Choice === "rock"){
-        winner.textContent = "Player 1 wins!";
+        win.textContent = "Player 1 wins!";
         player1Score++;
+        winner.textContent = "paper covers rock!";
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
     } else if(player1Choice === "scissors" && player2Choice === "paper") {
-        winner.textContent = "Player 1 wins!";
+        win.textContent = "Player 1 wins!";
         player1Score++;
+        winner.textContent = "scissors cut paper!";
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
     } else if (player2Choice === "rock" && player1Choice === "scissors"){
-        winner.textContent = "Player 2 wins!";
+        win.textContent = "Player 2 wins!";
         player2Score++;
+        winner.textContent = "rock crushes scissors!";
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
     } else if (player2Choice === "paper" && player1Choice === "rock"){
-        winner.textContent = "Player 2 wins!";
+        win.textContent = "Player 2 wins!";
         player2Score++;
+        winner.textContent = "paper covers rock!";
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
     } else if (player2Choice === "scissors" && player1Choice === "paper") {
-        winner.textContent = "Player 2 wins!";
+        win.textContent = "Player 2 wins!";
         player2Score++;
+        winner.textContent = "scissors cut paper!";
         p1score.textContent = "Player 1: " + player1Score.toString();
         p2score.textContent = "Player 2: " + player2Score.toString();
     }
@@ -140,20 +163,20 @@ function playRound(player1Choice, player2Choice) {
 
 function playGame(player1Choice, player2Choice) {
     playRound(player1Choice, player2Choice);
-    if (player1Score === 5 || player2Score === 5) {
+    if (player1Score >= 5 || player2Score >= 5) {
+        winner.textContent = "Press restart to play again.";
+    } else if (player1Score === 5 || player2Score === 5) {
         if (player1Score > player2Score) {
-            alert(`${player1Name} wins the game!`);
+            winner.textContent = ``;
+            win.textContent = `${player1Name} wins the game!`;
         } else if (player2Score > player1Score) {
-            alert(`${player2Name} wins the game!`);
+            winner.textContent = ``;
+            win.textContent = `${player2Name} wins the game!`;
         } else {
-            alert("It's a tie!");
+            winner.textContent = ``;
+            win.textContent = `it's a tie!`;
         }
-        resetGame();
-        player1Score = 0;
-        player2Score = 0;
-        p1score.textContent = "Player 1: " + player1Score.toString();
-        p2score.textContent = "Player 2: " + player2Score.toString();
-        winner.textContent = "Game Over!";
+        win.textContent = "Game Over!";
     }
 }
 
